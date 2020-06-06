21786e8b632b8149c3cc20ff9a06f0fadcb9d3f5
Kartikey Tanna <tannakartikey@gmail.com>
2020-06-06 23:51:05 +0530

    Added specs to assert all components rendering

--- src/components/App/App.test.js
+++ src/components/App/App.test.js
@@ -1,14 +1,38 @@
 import React from "react";
-import { render } from "@testing-library/react";
 import App from "./index";
-import { MemoryRouter } from "react-router-dom";
+import { Router } from "react-router-dom";
+import { render, fireEvent } from "@testing-library/react";
+import { createMemoryHistory } from "history";
 
-test("renders learn react link", () => {
-  const { getByText } = render(
-    <MemoryRouter>
+test("full app rendering/navigation", () => {
+  const history = createMemoryHistory();
+  const { container, getByText } = render(
+    <Router history={history}>
       <App />
-    </MemoryRouter>
+    </Router>
   );
-  const linkElement = getByText(/Aatm Nirbhar Bharat/i);
-  expect(linkElement).toBeInTheDocument();
+  expect(container.innerHTML).toMatch(/Aatm Nirbhar Bharat/);
+
+  fireEvent.click(getByText(/search/i));
+  expect(container.innerHTML).toMatch(/search code goes here/i);
+
+  fireEvent.click(getByText(/add/i));
+  expect(container.innerHTML).toMatch(/addproduct code goes here/i);
+
+  fireEvent.click(getByText(/faq/i));
+  expect(container.innerHTML).toMatch(/faq code goes here/i);
+
+  fireEvent.click(getByText(/about/i));
+  expect(container.innerHTML).toMatch(/about code goes here/i);
+});
+
+test("landing on a bad page redirects to Home", () => {
+  const history = createMemoryHistory();
+  history.push("/does/not/exist");
+  const { getByRole } = render(
+    <Router history={history}>
+      <App />
+    </Router>
+  );
+  expect(getByRole("heading")).toHaveTextContent(/Aatm Nirbhar Bharat/);
 });

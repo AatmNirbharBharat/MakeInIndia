060e5b35899a7da7cc417d57ecc7ba4aaa6ffe0a
Kartikey Tanna <tannakartikey@gmail.com>
2020-06-06 22:16:32 +0530

    Enabled strict mode

--- src/index.js
+++ src/index.js
@@ -6,11 +6,11 @@ import * as serviceWorker from "./serviceWorker";
 import { BrowserRouter } from "react-router-dom";
 
 ReactDOM.render(
-  // <React.StrictMode>
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>,
-  // </React.StrictMode>
+  <React.StrictMode>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </React.StrictMode>,
   document.getElementById("root")
 );
 

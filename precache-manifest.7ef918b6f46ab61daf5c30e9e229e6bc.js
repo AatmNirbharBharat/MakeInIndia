cd9446e7cd0ebbda3a9c125b7af270e2170e1561
tannakartikey <tannakartikey@gmail.com>
2020-06-07 11:57:58 +0000

    Deploying to gh-pages from  @ 479d6bd3bff91984edbef9931ba5c1fadc84f40f 🚀

---
+++ precache-manifest.7ef918b6f46ab61daf5c30e9e229e6bc.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7c4bc3c29c012474e1b8ce534df6a58a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3edacafb5f2b54cbadb3",
+    "url": "/static/css/main.95d06fb7.chunk.css"
+  },
+  {
+    "revision": "64ee186abcfe1a96dc58",
+    "url": "/static/js/2.80800976.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.80800976.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3edacafb5f2b54cbadb3",
+    "url": "/static/js/main.cd841de2.chunk.js"
+  },
+  {
+    "revision": "17ad6c506c9a97f6f7c9",
+    "url": "/static/js/runtime-main.b5d1174b.js"
+  },
+  {
+    "revision": "41d9f969830b95099904858d331105a8",
+    "url": "/static/media/Make_In_India.41d9f969.png"
+  }
+]);
